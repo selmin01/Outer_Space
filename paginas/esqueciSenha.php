c095bb438d4afb667217dd6114db7f4000083fa5
selmin01 <55216736+selmin01@users.noreply.github.com>
2019-09-17 22:02:00 -0300

    Atualizações de links/paginas

--- paginas/esqueciSenha.php
+++ paginas/esqueciSenha.php
@@ -2,54 +2,48 @@
     <head>
         <meta charset="utf-8">
         <title>Outer Space</title>
-        <link rel="stylesheet" href="../assets/css/bootstrap.min.css" />
-        <link rel="stylesheet" href="../assets/css/main.css">
+        <link rel="stylesheet" href="../estilo/css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="../estilo/css/main.css">
     </head>
     <body>
-    <div class="container">
-        <div class="mx-auto font box2">
-            <center><h1>Esqueci Minha Senha</h1></center>
-            <br>
-            <form action="index.php" method="post">
-                <div class="form-group row">
-                    <label class="col-sm-2 col-form-label">Nome:</label>
-                    <div class="col-sm-10 col-xs-3">
-	                <input type="text" class="form-control" name="nome" maxlength="50" required>
+        <div class="container">
+            <div class="mx-auto font box2">
+                <center><h1>Esqueci Minha Senha</h1></center>
+                <br>
+                <form action="../acao/alteraSenha.php" method="post">
+                    <div class="form-group">
+                        <label for="formGroupExampleInput">Nome:</label>
+                        <input type="text" class="form-control" name="nome" maxlength="50" required/>
                     </div>
-                </div>
-<!--
-                <div class="form-group row">
-                    <label class="col-sm-2 col-form-label">E-mail:</label>
-                    <div class="col-sm-10">
-                    <input type="email" class="form-control" name="email" maxlength="80" required> 
+    <!--
+                    <div class="form-group">
+                        <label for="formGroupExampleInput">E-mail:</label>
+                        <input type="email" class="form-control" name="email" maxlength="80" required/> 
                     </div>
-                </div>
--->
-                <div class="form-group row">
-                    <label class="col-sm-2 col-form-label">Usuário:</label>
-                    <div class="col-sm-10">
-                    <input type="text" class="form-control" name="nick" maxlength="50" required> 
+    -->
+                    <div class="form-group">
+                        <label for="formGroupExampleInput">Usuário:</label>
+                        <input type="text" class="form-control" name="nick" maxlength="50" required/>
                     </div>
-                </div>
 
-                <div class="form-group row">
-                    <label class="col-sm-2 col-form-label">Senha:</label>
-                    <div class="col-sm-10">
-                    <input type="password" class="form-control" name="senha" maxlength="50" required> 
+                    <div class="form-group">
+                        <label for="formGroupExampleInput">Senha:</label>
+                        <input type="password" class="form-control" name="senha" maxlength="50" required/> 
                     </div>
-                </div>
 
-                <div class="form-group row">
-                    <label class="col-sm-2 col-form-label">Confirmar Senha:</label>
-                    <div class="col-sm-10">
-                    <input type="password" class="form-control" name="confir_senha" maxlength="50" required> 
+                    <div class="form-group">
+                        <label for="formGroupExampleInput">Confirmar Senha:</label>
+                        <input type="password" class="form-control" name="confir_senha" maxlength="50" required/>
                     </div>
-                </div>
-            
-                <button type="submit" class="btn btn-primary btn-block">Alterar</button>
-            </form>
-            <button type="submit" class="btn btn-dark btn-block">Voltar</button>
+
+                    <a href="alteraSenha.php" class="linkbtn">
+                        <button type="submit" class="btn btn-primary btn-block">Alterar</button>
+                    </a>
+                </form>
+                <a href="login.php" class="linkbtn">
+                    <button type="submit" class="btn btn-dark btn-block">Voltar</button>
+                </a>
+            </div>
         </div>
-    </div>
     </body>
 </html>
---
+++ template/header.php
@@ -0,0 +1,6 @@
+<head>
+    <meta charset="utf-8">
+    <title>Outer Space</title>
+    <link rel="stylesheet" href="estilo/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="estilo/css/main.css">
+</head>
