da84ba12e3f60e10d502727db95642dfe727a2a1
Henrique-Santos <55415474+Henrique-Santos@users.noreply.github.com>
2019-09-24 20:50:05 -0300

    Ajustado função where criado funcões
    funcões de conectar ao banco e desconectar criadas, tmb de exclusão

--- bancoOuterSpace/banco.php
+++ bancoOuterSpace/banco.php
@@ -1,62 +1,47 @@
-<html>
-   <!-- <body>
-        <form action="#" method="post">
-            NOME<input type="text" name="nome"/> 
-            EMAIL<input type="email" name="email"/> 
-            USUARIO<input type="text" name="nick"/> 
-            SENHA<input type="password" name="senha"/>
-            <button type="submit" value="Enviar">Aperte</button>
-        </form>
-    </body> -->
-</html>
 <pre>
 <?php
 
-$conexao = mysqli_connect("localhost", "root", "", "outerspace");
-/*
-$idArray = selecionar("SELECT * FROM usuario WHERE nick = 'zika'");
-$id = implode("", $idArray);
-alterar('usuario', $_POST, $id);
-//inserir('usuario', $_POST);
-*/
-function inserir($tabela, $dados){
-    print_r($dados);
-    $campos = array_keys($dados);
-    print_r($campos);
-    $sql = "INSERT INTO ".$tabela." (";
+function onConexao() {
+    global $conexao;
+    $conexao = mysqli_connect("localhost", "root", "root", "outerspace");
+}
 
+function offConexao() {
+    global $conexao;
+    mysqli_close($conexao);
+}
+
+function executar($sql, $condicao = null){
+    global $conexao;
+    if(!empty($condicao)){
+        $sql.= " WHERE ". $condicao;
+    }
+    return mysqli_query($conexao, $sql);
+}
+
+function inserir($tabela, $dados){
+    $campos = array_keys($dados);
+    $sql = "INSERT INTO ".$tabela." (";
     // Monta a 1ª estrutura
     foreach($campos as $campo){
         $sql.=$campo.",";
     }
-    
     // Pega a string do $sql e exclui o último valor
     $sql = substr($sql,0, -1);
-
     $sql.=") VALUES (";
-
     // Monta a 2ª estrutura
     foreach($dados as $dado){
         $sql.="'".$dado."',";
     }
-
     $sql = substr($sql,0, -1);
-  
     $sql.=")";
-   
     $resultado = executar($sql);
-
     return $resultado;
 }
-// INSERT INTO orcamento VALUES(DEFAULT, '$cliente', $hora, $valorHora)
-// UPDATE orcamento SET cliente = '$cliente', horas = $totalHora, valorHora = $valorHora
 
-function alterar($tabela, $dados, $id) {
-
+function alterar($tabela, $dados, $condicao = null) {
     $campos = array_keys($dados);
-
     $sql = "UPDATE " .$tabela. " SET ";
-
     foreach($campos as $campo) {
         $sql.=$campo;
         foreach($dados as $dado) {
@@ -65,15 +50,8 @@ function alterar($tabela, $dados, $id) {
             break;
         }
     }
-print_r($dados);
     $sql = substr($sql, 0, -2);
-
-    $sql.= " WHERE idUsuario = ". $id;
-
-    echo $sql;
-
-    $resultado = executar($sql);
-
+    $resultado = executar($sql,$condicao);
     return $resultado;
 }
 
@@ -83,22 +61,18 @@ function selecionar($sql){
     return $arrayDados;
 }
 
+function apagar($tabela, $condicao = null) {
+    $sql = "DELETE FROM ". $tabela;
+    $resultado = executar($sql, $condicao);
+    return $resultado;
+}
+
+/*
 function selecionarId($sql){
     $arrayDados = selecionar($sql);
     $id = array_column($arrayDados, 'idUsuario');
     return $id;
 }
-
-function executar($sql, $where=null){
-
-    global $conexao;
-
-    if(!empty($where)){
-        $sql.= " WHERE ". $where;
-    }
-
-    return mysqli_query($conexao, $sql);
-}
-
+*/
 ?>
 </pre>
