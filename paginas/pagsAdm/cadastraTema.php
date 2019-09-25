fbb74ce439742789216977287c3ff8c9870e7633
bresquedele <55216903+bresquedele@users.noreply.github.com>
2019-09-24 21:51:54 -0300

    tela cadastra tema e pergunta
    troquei assets por estilo

---
+++ paginas/pagsAdm/cadastraTema.php
@@ -0,0 +1,63 @@
+<html>
+<head>
+    <link rel="stylesheet" href="../../estilo/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="../../estilo/css/main.css"/>
+<head>
+<body>
+<div class="container">
+    <div class="mx-auto cadastroTema">
+        <center><h2>Cadastro do Tema</h2></center>
+        <br>
+        <form action="" method="post">
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label">Tema:</label>
+                <div class="col-sm-10">
+                <input type="text" class="form-control" name="tema" maxlength="" required> 
+                </div>
+            </div>
+            <div>
+                <center><h2>Cadastrar Pergunta</h2></center>
+                <br>
+            </div>
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label">Pergunta:</label>
+                <div class="col-sm-10">
+                <input type="text" class="form-control" name="pergunta" maxlength="" required> 
+                </div>
+            </div>
+            <div>
+                <center><h5>Resposta:</h5></center>
+                <br>
+            </div>
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label">A</label>
+                <div class="col-sm-10">
+                <input type="text" class="form-control" name="respostaA" maxlength="" required> 
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label">B</label>
+                <div class="col-sm-10">
+                <input type="text" class="form-control" name="respostaB" maxlength="" required> 
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label">C</label>
+                <div class="col-sm-10">
+                <input type="text" class="form-control" name="respostaC" maxlength="" required> 
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label">D</label>
+                <div class="col-sm-10">
+                <input type="text" class="form-control" name="respostaD" maxlength="" required> 
+                </div>
+            </div>
+            <button type="submit" class="btn btn-success btn-block">Cadastrar</button>
+        </form>
+            <button type="button" class="btn btn-dark btn-block">Voltar</button>
+    </div>
+</div>
+</body>
+
+</html>
