472574589553022ab5c2db5319ce3600db99bbb2
Kuramoto007 <55216617+Kuramoto007@users.noreply.github.com>
2019-09-25 22:16:29 -0300

    criaçao tela ranking e botao tela menu arrumado

--- paginas/ranking.php
+++ paginas/ranking.php
@@ -5,13 +5,67 @@
 
 ?>
 
+
 <html>
-
-<div class="mx-auto caixa">
-
-<center>
-<h1>TELA DO RANKING</h1>
-</center>
-</div>
-
+    <head>
+        <meta charset="utf-8">
+        <title>Outer Space</title>
+        <link rel="stylesheet" href="../estilo/css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="../estilo/css/main.css"/>
+        <link href="https://fonts.googleapis.com/css?family=Orbitron&display=swap" rel="stylesheet"/>
+    </head>
+    <body>
+        <div class="container">
+            <div class="mx-auto caixa">
+                <center><h1 class="font">RANKING</h1></center>
+                <br>
+                <div>
+                    <label>Pos</label>
+                    <label class="rnk1">Nome do Jogador</label>
+                    <label class="rnk2">Pontuação</label>
+                </div> 
+                <div>
+                    <label>1º</label>
+                    <label class="rnk3">H3nr1qu3_z1k4</label>
+                    <label class="rnk4">9999</label>
+                    <br>
+                    <label>2º</label>
+                    <label class="rnk3">Ricardin_Gameplays</label>
+                    <label class="rnk4">8888</label>
+                    <br>
+                    <label>3º</label>
+                    <label class="rnk3">Carlao_Dale</label>
+                    <label class="rnk4">7777</label>
+                    <br>
+                    <label>4º</label>
+                    <label class="rnk3">Robertaum_Milos</label>
+                    <label class="rnk4">6666</label>
+                    <br>
+                    <label>5º</label>
+                    <label class="rnk3">Selmin_Br</label>
+                    <label class="rnk4">5555</label>
+                    <br>
+                    <label>6º</label>
+                    <label class="rnk3">Felipe_em_HD</label>
+                    <label class="rnk4">4444</label>
+                    <br>
+                    <label>7º</label>
+                    <label class="rnk3">Koerber_Gui</label>
+                    <label class="rnk4">3333</label>
+                    <br>
+                    <label>8º</label>
+                    <label class="rnk3"> Caio_Bolinho_deCarne</label>
+                    <label class="rnk4">2222</label>
+                    <br>
+                    <label>9º</label>
+                    <label class="rnk3">CoIsA_lInDa</label>
+                    <label class="rnk4">1111</label>
+                    <br>
+                    <label>10º</label>
+                    <label class="rnk3">Xesquedele</label>
+                    <label class="rnk4">0000</label>
+                </div> 
+            </div>
+        </div>
+    </body>
 </html>
