92e0d43fd97420167ce46cbeabd8697a3c7c9268
selmin01 <55216736+selmin01@users.noreply.github.com>
2019-09-26 22:00:44 -0300

    git merdaa

--- paginas/pagsAdm/cadastraTema.php
+++ paginas/pagsAdm/cadastraTema.php
@@ -1,66 +1,58 @@
-<html>
-<head>
-    <link rel="stylesheet" href="../../estilo/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="../../estilo/css/main.css"/>
-<head>
-<body>
-<div class="container">
-    <div class="mx-auto font">
-        <center><h2 calss="">Cadastro do Tema</h2></center>
-        <br>
-        <form action="../../acao/cadastraPergunta.php" method="post">
-            <div class="boxTema">
-                <div class="form-group row">
-                    <label class="col-sm-2 col-form-label">Tema:</label>
-                    <div class="col-sm-10">
-                    <input type="text" class="form-control" name="tema" maxlength="" required> 
+<html><head>
+        <link rel="stylesheet" href="../../estilo/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../../estilo/css/cadastroTema.css">
+        <link href="https://fonts.googleapis.com/css?family=Orbitron&amp;display=swap" rel="stylesheet">
+    
+    </head><body>
+        <div class="container">
+            <form action="../../acao/cadastraPergunta.php" method="post">
+                <div class="row font">
+                    <div class="col-md-12 mt-5">
+                        <h2>Cadastro de Perguntas</h2>
                     </div>
-                <div>
-                    <center><h2>Cadastrar Pergunta</h2></center>
-                    <br>
-                </div>
-                <div class="form-group row">
-                    <label class="col-sm-2 col-form-label">Pergunta:</label>
-                    <div class="col-sm-10">
-                    <input type="text" class="form-control" name="pergunta" maxlength="" required> 
+                    <div class="col-md-6 boxTema">
+                        <div class="form-group ">
+                            <label for="formGroupExampleInput">Tema:</label>
+                            <input type="text" class="form-control" name="tema" maxlength="" required=""> 
+                        </div>
+                        <div class="form-group">
+                            <label for="formGroupExampleInput">Pergunta:</label>
+                            <input type="text" class="form-control" name="pergunta" maxlength="" required="">
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div class="boxResposta">
-                <div>
-                    <center><h5>Resposta:</h5></center>
-                    <br>
-                </div>
-                <div class="form-group row">
-                    <label class="col-sm-2 col-form-label">A</label>
-                    <div class="col-sm-10">
-                    <input type="text" class="form-control" name="respostaA" maxlength="" required> 
+                    <div class="col-md-6 boxResposta">
+                        <h5>Resposta:</h5>
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">A</label>
+                            <div class="col-sm-10">
+                            <input type="text" class="form-control" name="respostaA" maxlength="" required=""> 
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">B</label>
+                            <div class="col-sm-10">
+                            <input type="text" class="form-control" name="respostaB" maxlength="" required=""> 
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">C</label>
+                            <div class="col-sm-10">
+                            <input type="text" class="form-control" name="respostaC" maxlength="" required=""> 
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">D</label>
+                            <div class="col-sm-10">
+                            <input type="text" class="form-control" name="respostaD" maxlength="" required=""> 
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div class="form-group row">
-                    <label class="col-sm-2 col-form-label">B</label>
-                    <div class="col-sm-10">
-                    <input type="text" class="form-control" name="respostaB" maxlength="" required> 
+                    <div class="col-md-12 pt-5">
+                        <button type="submit" class="btn btn-dark btn-lg btn-block font">Cadastrar</button>
+                        <button type="button" class="btn btn-dark btn-lg btn-block font">Voltar</button>
                     </div>
-                </div>
-                <div class="form-group row">
-                    <label class="col-sm-2 col-form-label">C</label>
-                    <div class="col-sm-10">
-                    <input type="text" class="form-control" name="respostaC" maxlength="" required> 
-                    </div>
-                </div>
-                <div class="form-group row">
-                    <label class="col-sm-2 col-form-label">D</label>
-                    <div class="col-sm-10">
-                    <input type="text" class="form-control" name="respostaD" maxlength="" required> 
-                    </div>
-                </div>
-            </div>
-            <button type="submit" class="btn btn-dark btn-lg btn-block font">Cadastrar</button>
-        </form>
-            <button type="button" class="btn btn-dark btn-lg btn-block font">Voltar</button>
-    </div>
-</div>
-</body>
-
-</html>
+                </div>           
+            </form>
+        </div>
+    
+</body></html>
