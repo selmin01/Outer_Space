d400acfb543169e41153f1d02b39957e3bd4008c
selmin01 <55216736+selmin01@users.noreply.github.com>
2019-09-30 20:34:09 -0300

    tela Linda (cadastraTema)

--- paginas/pagsAdm/cadastraTema.php
+++ paginas/pagsAdm/cadastraTema.php
@@ -8,21 +8,22 @@
     <body>
         <div class="container">
             <form action="../../acao/cadastraPergunta.php" method="post">
-                <div class="row font">
-                    <div class="col-md-12 mt-5">
+                <div class="row">
+                    <div class="col-md-12 mt-5 font titulo">
                         <h2>Cadastro de Perguntas</h2>
+                        <br>
                     </div>
-                    <div class="col-md-6 boxTema">
+                    <div class="col-md-6 boxTema font">
                         <div class="form-group ">
-                            <label for="formGroupExampleInput">Tema:</label>
+                            <label for="formGroupExampleInput"><h5>Tema:</h5></label>
                             <input type="text" class="form-control" name="tema" maxlength="" required=""> 
                         </div>
                         <div class="form-group">
-                            <label for="formGroupExampleInput">Pergunta:</label>
+                            <label for="formGroupExampleInput"><h5>Pergunta:</h5></label>
                             <input type="text" class="form-control" name="pergunta" maxlength="" required="">
                         </div>
                     </div>
-                    <div class="col-md-6 boxResposta">
+                    <div class="col-md-6 boxResposta font">
                         <h5>Resposta:</h5>
                         <div class="form-group row">
                             <label class="col-sm-2 col-form-label">A</label>
@@ -49,7 +50,7 @@
                             </div>
                         </div>
                         <div>
-                            <label>Selecione a Resposta Certa</label>
+                            <label><h5>Selecione a Resposta Certa</h5></label>
                             <select name="select" class="select">
                                 <option value="A">A</option>
                                 <option value="B">B</option>
@@ -58,7 +59,7 @@
                             </select>
                         </div>
                     </div>
-                    <div class="col-md-12 pt-5">
+                    <div class="col-md-12 pt-2">
                         <button type="submit" class="btn btn-dark btn-lg btn-block font">Cadastrar</button>
                         <button type="button" class="btn btn-dark btn-lg btn-block font">Voltar</button>
                     </div>
