42b7363d7bb07389e8217bfe659f4c61ef4b6950
Henrique-Santos <55415474+Henrique-Santos@users.noreply.github.com>
2019-10-04 17:32:35 -0300

    grupo
    testes

--- acao/adicionaGrupo.php
+++ acao/adicionaGrupo.php
@@ -7,20 +7,22 @@ $dados = $_SESSION["post"];
 
 print_r($dados);
 
-$grupo = $dados["descricaoGrupo"];
+$grupo = array($dados["descricaoGrupo"]);
 print_r($grupo);
 $membro = $dados["usuario_idUsuario"];
 print_r($membro);
 
 
-$dadosRanking = array("usuario_idUsuario" => $membro, "ponto" => 0);
+$dadosRanking = array("usuario" => $membro, "ponto" => 0);
 print_r($dadosRanking);
+
 
 onConexao();
 
 $idRanking = inserir('rankinggrupo', $dadosRanking);
+print_r($idRanking);
 
-$dadosGrupo = array("rankingGrupo_idRankingGrupo" => $idRanking, "descricaoGrupo" => $grupo);
+$dadosGrupo = array("rankingGrupo_idRankingGrupo" => $idRanking["id"], "descricaoGrupo" => $dados["descricaoGrupo"]);
 print_r($dadosGrupo);
 
 $idGrupo = inserir('grupo', $grupo, $idRanking);
@@ -33,8 +35,8 @@ $idUsuario = implode("", $idUsuario);
 
 $dadosUsuarioGrupo = array("usuario_idUsuario" => $idUsuario, "grupo_idGrupo" => $idGrupo);
 print_r($dadosUsuarioGrupo);
-
-inserir('usuariogrupo', $membro, $idGrupo);
+$usuarioGrupo = array("usuario_idUsuario" => $idUsuario, "grupo_idGrupo" => $idGrupo);
+inserir('usuariogrupo', $usuarioGrupo);
 
 offConexao();
 
