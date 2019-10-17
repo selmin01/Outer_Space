b51ab9d091a30cbc93d40fcfe46826828413b755
selmin01 <55216736+selmin01@users.noreply.github.com>
2019-10-16 21:49:01 -0300

    modificacoes no grupo/login

--- acao/validaLogin.php
+++ acao/validaLogin.php
@@ -6,22 +6,15 @@ $usuario = array("nick"=>$nick, "senha"=>$senha);
 
 include "../bancoOuterSpace/banco.php";
 onConexao();
-selecionar("SELECT * FROM usuario WHERE senha='$senha' and nick='$nick'");
-$sucesso = mysqli_affected_rows($conexao);
+$arrayUsu = selecionar("SELECT * FROM usuario WHERE senha='$senha' and nick='$nick'");
 offConexao();
 
-
-if( isset($_SESSION["usuario"])){
-    $_SESSION["usuario"] [] = $usuario;
+if(count($arrayUsu)==1){
+    $_SESSION["usuario"] = $usuario;
     header("Location: ../paginas/menu.php");
 }else{
-    $_SESSION["usuario"] = array($usuario);
-    header("Location: ../paginas/menu.php");
-}
-
-if($sucesso >= 1) {
-    header("Location: ../paginas/menu.php");
-}else {
+    unset($_SESSION["usuario"]);
     header("Location: ../paginas/login.php?msg=5");
 }
+//print_r( $_SESSION["usuario"]);
 ?>
