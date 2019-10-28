406a9b719768b46f4ac2383559845b06992447b5
selmin01 <55216736+selmin01@users.noreply.github.com>
2019-10-28 20:27:07 -0300

    senha do banco

--- bancoOuterSpace/banco.php
+++ bancoOuterSpace/banco.php
@@ -1,7 +1,7 @@
 <?php
 function onConexao() {
     global $conexao;
-    $conexao = mysqli_connect("localhost","root", "root","outerspace");
+    $conexao = mysqli_connect("localhost","root","","outerspace");
 }
 
 function offConexao() {
