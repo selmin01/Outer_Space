5d27d6115bbb4ba8c736a46d796e514330727229
Kuramoto007 <55216617+Kuramoto007@users.noreply.github.com>
2019-11-04 22:12:14 -0300

    Persistencia e Valida Login
    Persistencia Do usuário completo;
    Pag. ValidaLogin arrumada quase toda.

--- acao/validaLogin.php
+++ acao/validaLogin.php
@@ -4,22 +4,32 @@ $nick = isset($_POST["nick"]) ? $_POST["nick"] : "";
 $senha = isset($_POST["senha"]) ? $_POST["senha"] : "";
 
 include "../bancoOuterSpace/banco.php";
+
 onConexao();
+
 $arrayUsu = selecionar("SELECT * FROM usuario WHERE senha='$senha' and nick='$nick'");
+
 offConexao();
-//print_r($arrayUsu);
-$id_usuario = $arrayUsu [0]["idUsuario"];
-$permissao = $arrayUsu [0]["permissao"];
-$usuario = array("idUsuario"=>$id_usuario ,"nick"=>$nick, "senha"=>$senha, "permissao"=>$permissao);
-//print_r($usuario);
-if ($usuario["permissao"] == 1){
-    header("Location: sessaoAdm.php");
-}else if(count($arrayUsu)==1){
-    $_SESSION["usuario"] = $usuario;
-    header("Location: ../paginas/menu.php");
-}else{
-    unset($_SESSION["usuario"]);
+
+if(empty($arrayUsu)) {
     header("Location: ../paginas/login.php?msg=5");
+}else {
+    $id_usuario = $arrayUsu[0]["idUsuario"];
+
+    $permissao = $arrayUsu[0]["permissao"];
+
+    $nick = $arrayUsu[0]["nick"];
+
+    $senha = $arrayUsu[0]["senha"];
+
+    $usuario = array("idUsuario"=>$id_usuario ,"nick"=>$nick, "senha"=>$senha, "permissao"=>$permissao);
+    
+    $_SESSION["usuario"] = $usuario;
+
+    if($usuario["permissao"] == 1) {
+        header("Location: ../paginas/pagsAdm/menuAdm.php");
+    }else {
+        header("Location: ../paginas/menu.php");
+    }
 }
-//print_r( $_SESSION["usuario"]);
 ?>
