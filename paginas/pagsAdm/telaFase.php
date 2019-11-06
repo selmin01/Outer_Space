e921637e32e4436258db6c795cad85c22cd0eb9e
Kuramoto007 <55216617+Kuramoto007@users.noreply.github.com>
2019-11-05 22:10:43 -0300

    ajustes permissoes e estilização da tela de cadastrar fase

--- paginas/pagsAdm/telaFase.php
+++ paginas/pagsAdm/telaFase.php
@@ -18,10 +18,10 @@ include "../../acao/persistenciaGrupo.php";
 		<div class="container">
             <form action="../../acao/cadastraFase.php" method="post">
                 <div class="row">
-                    <div class="col-md-12 mt-4 md-6 mx-auto font">
+                    <div class="col-md-12 mt-2 md-12 mx-auto font">
                         <div class="mx-auto" style="width: 350px;"><h2>Cadastro de Fase</h2></div>
                     </div>
-                    <div class="col-md-" >
+                    <div class="col-md-12" >
                         <div class="form-group ">
                             <label for="formGroupExampleInput"><h5 class="font ">Velocidade da Nave</h5></label>
                             <select name="velocidadeNave" class="form-control vel" required="">
@@ -33,16 +33,18 @@ include "../../acao/persistenciaGrupo.php";
 								<option value="5">5</option>
                             </select>
                         </div>
-                        <div class="form-group">
-                            <label for="formGroupExampleInput"><h5 class="font lado">Quantidade de meteoros</h5></label>
+                        <div class="form-group row">
+                            <label class="col-sm-10 col-form-label font"><h5>Quantidade de meteoros</h5></label>
+                            <div class="col-sm-10">
                             <input type="number" class="meteoro" name="qtdMeteoro">
+                            </div>
                         </div>
-                        <div class="form-group">
-                            <label for="formGroupExampleInput"><h5 class="font lado">Tempo da Fase ( Em Minutos ) </h5></label>
+                        <div class="form-group row">
+                            <label for="formGroupExampleInput"><h5 class="font">Tempo da Fase ( Em Minutos ) </h5></label>
+                            <div class="col-sm-10">
                             <input type="time" class="tempo" name="tempo" min="00:01:00" max="00:30:00">
+                            </div>
                         </div>
-                    </div>
-                    
                     </div>
                         <div class="col-md-8 botoes">
                         <button type="submit" class="btn btn-dark btn-lg btn-block font">Cadastrar</button> 
