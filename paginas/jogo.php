cf72650237b74f0d41bb0fecc09ab2b7e61747ab
selmin01 <55216736+selmin01@users.noreply.github.com>
2019-11-06 22:18:29 -0300

    jogofoda

--- paginas/jogo.php
+++ paginas/jogo.php
@@ -1,13 +1,18 @@
 <?php
 include "../acao/persistenciaUsuario.php";
 ?>
-
 <html>
   <head>
     <meta charset="utf-8">
     <title>Outer Space</title>
     <link rel="stylesheet" href="../estilo/css/main.css"/>
     <script type="text/javascript" src="../js/script.js"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script>
+      $(document).ready(function(){
+        start();
+      })
+    </script>
   </head>
   <body>
     <div id="painel">
@@ -19,11 +24,12 @@ include "../acao/persistenciaUsuario.php";
       <div id="contBombas"></div>
     </div>
     <!-- Mostra a imagem de vitoria ou derrota -->
+    <!--
     <div id="telaMsg" class="telaMsg">
       <button id="btnJogar" class="btnJogar">Jogar</button>
     </div>
+    -->
     <!-- Nave a ser controlada -->
     <div id="naveJog" class="naveJog"></div>
-
     </body>
 </html>
