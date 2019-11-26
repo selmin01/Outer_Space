ed9b6cf154416b12cd3e5fea6349364d20feab2a
selmin01 <55216736+selmin01@users.noreply.github.com>
2019-11-25 21:55:36 -0300

    mais um

--- acao/acaoJogo/req_pontuacao.php
+++ acao/acaoJogo/req_pontuacao.php
@@ -5,8 +5,9 @@ include "../../bancoOuterSpace/banco.php";
 if(isset($_SESSION["usuario"])){
     $idUsuario = $usuario["idUsuario"];
     $pont = $_POST["pont"];
-    $dados= array(0=>$idUsuario, 1=>$pont);
-    aleterar("usuario",$dados):
+    $dados= array("idUsuario"=>$idUsuario, "pontos"=>$pont);
+    //print_r($dados);
+    aleterar("usuario",$dados,"idUsuario=".$idUsuario):
 }
 
 echo $pont;
