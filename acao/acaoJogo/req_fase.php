7373c5484af34dc9adeccd39aa3f6a13f017d869
selmin01 <55216736+selmin01@users.noreply.github.com>
2019-12-02 22:16:32 -0300

    mais um

--- acao/acaoJogo/req_fase.php
+++ acao/acaoJogo/req_fase.php
@@ -3,8 +3,8 @@ if(isset($_SESSION["usuario"])){
     $idUsuario=$usuario["idUsuario"];
 
     onConexao();
-    $arrayFase = selecionar("SELECT idFase, maxPontFase, qtdMeteoro, velocidadeNave  FROM fase 
-    INNER JOIN usuario WHERE idUsuario = '$idUsuario'");
+    $arrayFase = selecionar("SELECT idFase, maxPontFase, qtdMeteoro, velocidadeNave FROM fase 
+    INNER JOIN usuario ON fase.idfase = usuario.fase_idFase WHERE idUsuario = '$idUsuario'");
     offConexao();
 
     $idFase = $arrayFase[0]["idFase"];
