2e97d34d8ebc5e5efcc133f7f67c53f1694a673b
Henrique-Santos <55415474+Henrique-Santos@users.noreply.github.com>
2019-12-03 22:15:14 -0300

    sprite
    ok

--- acao/acaoJogo/req_fase.php
+++ acao/acaoJogo/req_fase.php
@@ -3,15 +3,16 @@ if(isset($_SESSION["usuario"])){
     $idUsuario=$usuario["idUsuario"];
 
     onConexao();
-    $arrayFase = selecionar("SELECT idFase, maxPontFase, qtdMeteoro, velocidadeNave FROM fase 
-    INNER JOIN usuario ON fase.idfase = usuario.fase_idFase WHERE idUsuario = '$idUsuario'");
+    
+    $arrayFase = selecionar("SELECT idFase, maxPontFase, qtdMeteoro, velocidadeNave 
+                             FROM fase  INNER JOIN usuario 
+                             ON fase.idfase = usuario.fase_idFase WHERE idUsuario = '$idUsuario'");
     offConexao();
 
     $idFase = $arrayFase[0]["idFase"];
     $pontFase = $arrayFase[0]["maxPontFase"];
     $qtdMeteoro = $arrayFase[0]["qtdMeteoro"];
     $velocidadeNave = $arrayFase[0]["velocidadeNave"];
-
-    //header("Location: ../../paginas/jogo.php");
 }
+  
 ?>
